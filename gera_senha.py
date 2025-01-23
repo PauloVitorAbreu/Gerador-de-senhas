966c311ebf3a4f063dd966a9625d596658d71c94
Paulo Vitor Dos Santos Abreu <paulovitordossantosabreu@gmail.com>
2025-01-23 18:46:48 -0300

    Add files via upload
    **Descrição do Repositório:**  
    *"Um gerador de senhas em Python com funcionalidades personalizáveis para maior segurança e flexibilidade. Este projeto permite criar senhas de qualquer comprimento, com a opção de incluir caracteres especiais e letras em maiúsculas (CAPSLOCK). Ideal para quem precisa de uma ferramenta prática e confiável para criar combinações fortes e únicas."*
    
    **Destaques do projeto:**
    - O usuário define o comprimento da senha.
    - Escolha entre incluir ou não caracteres especiais (ex.: `!@#$%^&*`).
    - Opção para alternar entre letras minúsculas e maiúsculas aleatoriamente.
    - Gera senhas rapidamente usando bibliotecas padrão como `random` e `string`.

---
+++ gera_senha.py
@@ -0,0 +1,57 @@
+#criador de senhas
+#perguntas: comprimento_senha, caracter_esp, CAPS(true ou falso), (26)
+import random as r
+import string as s
+
+def cria_senha (comprimento_senha, caracter_esp, caps):
+    alfabeto = list(s.ascii_lowercase)
+    ca_esp = '!@#$%^&*()-_=+[]{};:\",.<>?/|\\'
+    lista_esp = list(ca_esp)
+    contador = 0
+    senha = ''
+
+
+    while contador != comprimento_senha:
+        escolhe_posicao = r.randint(0, 25)
+        pega_letra = alfabeto[escolhe_posicao]
+        
+        if caps == True:
+            sorteia = r.randint(0,3)
+            if sorteia/2 != 0:
+                pega_letra = pega_letra.upper()
+                senha += pega_letra 
+            else:
+                senha += pega_letra 
+
+        if caracter_esp == True:
+            sorteia_2 = r.randint(0,3)
+            if sorteia_2/2 == 0:
+                pos_esp = r.randint(0,28)
+                pega_esp = lista_esp[pos_esp]
+                senha += pega_esp
+            else:
+                senha += pega_letra
+
+        else:
+            senha += pega_letra 
+
+        contador += 1
+
+    return senha
+
+comprimento_senha = int(input('quantos caracteres sua senha precisa ter: '))
+
+caracter_esp = input('Diga em "sim" ou "não" se deseja incluir caracter especiais: ')
+if caracter_esp == 'sim':
+    caracter_esp = True
+else:
+    caracter_esp = False
+
+caps = input('Diga em "sim" ou "não" se deseja incluir caracter em CAPSLOCK: ')
+if caps == 'sim':
+    caps = True
+else:
+    caps = False
+
+resultado = cria_senha(comprimento_senha, caracter_esp, caps)
+print(resultado)
